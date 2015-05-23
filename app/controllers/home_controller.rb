433c7368df1eec1ca65dd1c767559395fa88bcb6
Andrew Butcher <abutcher@redhat.com>
2015-05-23 19:32:18 -0400

    Tie the pages together.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
   def index
+    @pokemons = current_user.pokemon
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
+  get '/pokemon/new' => 'pokemons#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
